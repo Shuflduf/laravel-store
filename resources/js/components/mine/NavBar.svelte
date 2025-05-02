43b44a1659a0c9df0f75c5304f1f78298e897868
Shuflduf <shuflduf@shuflduf.xyz>
2025-05-02 13:08:45 -0600

    idk

--- resources/js/components/mine/NavBar.svelte
+++ resources/js/components/mine/NavBar.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-    import { Link } from "@inertiajs/svelte";
-import Button from "../ui/button/button.svelte";
+    import { Link, page } from "@inertiajs/svelte";
+    import Button from "../ui/button/button.svelte";
     import StyledLink from "./StyledLink.svelte";
+    import { onMount } from "svelte";
+    import { User } from '@geist-ui/icons';
+
+    onMount(() => {
+        let user = $page.props.auth.user;
+        console.log(user.name)
+    });
 </script>
 
 <div class="flex justify-between fixed top-0 w-screen border-b-zinc-600 border-b-1 bg-zinc-800/50 backdrop-blur-md">
@@ -11,8 +18,17 @@ import Button from "../ui/button/button.svelte";
         <StyledLink href={route('testing')} children="Shop" />
     </div>
     <div class="flex gap-4 m-2 justify-end w-1/4">
-        <Link href={route('login')} >
-            <Button variant="secondary">Login / Sign Up</Button>
-        </Link>
+        {#if $page.props.auth.user}
+            <Link href={route('dashboard')} >
+                <Button class="flex gap-2">
+                    <User />
+                    {$page.props.auth.user.name}
+                </Button>
+            </Link>
+        {:else}
+            <Link href={route('login')} >
+                <Button variant="primary">Login</Button>
+            </Link>
+        {/if}
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@ Route::get('/welcome', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/* Route::get('dashboard', function () { */
+/*     return Inertia::render('Dashboard'); */
+/* })->middleware(['auth', 'verified'])->name('dashboard'); */
+
+Route::redirect('/dashboard', '/')->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
